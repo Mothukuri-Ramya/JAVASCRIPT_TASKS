2c1345f65f0b91d4afc5101bf6ba358aeca0a0c7
Ramya-Mothukuri <o180301@rguktong.ac.in>
2024-06-06 16:13:20 +0530

    Basic Array Task Done

---
+++ Task11.js
@@ -0,0 +1,14 @@
+//a.Define a variable named colors and assign it an array containing strings representing different colors (e.g., "red", "green", "blue").
+let colors=["red","green","blue"];
+
+//b.Log the first element of the colors array to the console.
+console.log(colors[0]);
+
+//c.Change the second element of the colors array to "yellow". Log the second element of the colors array to the console to verify the change.
+colors[1]="yellow";
+console.log(colors[1]);
+
+//d.Add a new color to the end of the colors array. Log the last element of the colors array to the console.
+colors.push("white");
+console.log(colors[colors.length-1]);
+
